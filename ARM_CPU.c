cb1c2d239bfebfbe783548e55abaf2bf68f90803
IComplainInComments <Optachan@outlook.com>
2024-03-07 19:36:06 -0500

    Descriptive Comment

--- ARM_CPU.c
+++ ARM_CPU.c
@@ -13,7 +13,7 @@ void dec2bin(uint64_t c){
    }
 }
 
-void read_id_pfr0(uint64_t* value) {
+void dump_registers(uint64_t* value) {
     asm inline("mrs %0, ID_AA64PFR0_EL1" : "=r" (value[0]));
     asm inline("mrs %0, ID_AA64PFR1_EL1" : "=r" (value[1]));
     asm inline("mrs %0, ID_AA64AFR0_EL1" : "=r" (value[2]));
@@ -21,47 +21,43 @@ void read_id_pfr0(uint64_t* value) {
     asm inline("mrs %0, ID_AA64MMFR0_EL1" : "=r" (value[4]));
     asm inline("mrs %0, ID_AA64MMFR1_EL1" : "=r" (value[5]));
     asm inline("mrs %0, ID_AA64MMFR2_EL1" : "=r" (value[6]));
-    asm inline("mrs %0, ID_AA64MMFR3_EL1" : "=r" (value[7]));
-    asm inline("mrs %0, ID_AA64ISAR0_EL1" : "=r" (value[8]));
-    asm inline("mrs %0, ID_AA64ISAR1_EL1" : "=r" (value[9]));
-    asm inline("mrs %0, ID_AA64ISAR2_EL1" : "=r" (value[10]));
+    asm inline("mrs %0, ID_AA64ISAR0_EL1" : "=r" (value[7]));
+    asm inline("mrs %0, ID_AA64ISAR1_EL1" : "=r" (value[8]));
+    asm inline("mrs %0, ID_AA64ISAR2_EL1" : "=r" (value[9]));
 }
 
 int main() {
-    uint64_t id_pfr0[12];
-    read_id_pfr0(id_pfr0);
+    uint64_t register_info[10];
+    dump_registers(register_info);
     printf("ID_PFR0 register value: ");
-    dec2bin(id_pfr0[0]);
+    dec2bin(register_info[0]);
     printf("\n");
     printf("ID_PFR1 register value: ");
-    dec2bin(id_pfr0[1]);
+    dec2bin(register_info[1]);
     printf("\n");
     printf("ID_AFR0 register value: ");
-    dec2bin(id_pfr0[2]);
+    dec2bin(register_info[2]);
     printf("\n");
     printf("ID_DFR0 register value: ");
-    dec2bin(id_pfr0[3]);
+    dec2bin(register_info[3]);
     printf("\n");
     printf("ID_MMFR0 register value: ");
-    dec2bin(id_pfr0[4]);
+    dec2bin(register_info[4]);
     printf("\n");
     printf("ID_MMFR1 register value: ");
-    dec2bin(id_pfr0[5]);
+    dec2bin(register_info[5]);
     printf("\n");
     printf("ID_MMFR2 register value: ");
-    dec2bin(id_pfr0[6]);
-    printf("\n");
-    printf("ID_MMFR3 register value: ");
-    dec2bin(id_pfr0[7]);
+    dec2bin(register_info[6]);
     printf("\n");
     printf("ID_ISAR0 register value: ");
-    dec2bin(id_pfr0[8]);
+    dec2bin(register_info[7]);
     printf("\n");
     printf("ID_ISAR1 register value: ");
-    dec2bin(id_pfr0[9]);
+    dec2bin(register_info[8]);
     printf("\n");
     printf("ID_ISAR2 register value: ");
-    dec2bin(id_pfr0[10]);
+    dec2bin(register_info[9]);
     printf("\n");
     return 0;
 }
